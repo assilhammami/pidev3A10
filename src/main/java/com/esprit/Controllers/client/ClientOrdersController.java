a26f4e9ee622746216ac6c42d2c1c8ad10be36ff
oumaimagaidi <100679152+oumaimagaidi@users.noreply.github.com>
2024-02-28 21:54:50 +0100

    Add files via upload

--- src/main/java/com/esprit/Controllers/client/ClientOrdersController.java
+++ src/main/java/com/esprit/Controllers/client/ClientOrdersController.java
@@ -1,4 +1,4 @@
-package com.esprit.Controllers.client;
+package com.esprit.Controllers.client.orders;
 
 import com.esprit.Controllers.admin.OrderItemController;
 import com.esprit.models.Commande;
@@ -12,11 +12,15 @@ import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -28,20 +32,48 @@ import java.util.ResourceBundle;
 public class ClientOrdersController implements Initializable {
 
     @FXML
-    private VBox chosen;
+    private Label Date_commande;
+
+    @FXML
+    private ImageView Image_produit;
+
+    @FXML
+    private Label Montant_total;
+
+    @FXML
+    private Label Nom_produit;
+
+    @FXML
+    private ChoiceBox<?> filtreOptions;
 
     @FXML
     private GridPane grid;
 
     @FXML
     private ScrollPane scroll;
-    private List<Commande> commandesList = new ArrayList<>();
+
+    @FXML
+    private TextField searchInput;
+    int id_cmd;
+
     MyListenerCommande myListenerCommande;
 
     public Commande selectedOrder;
-
+    private List<Commande> commandesList = new ArrayList<>();
     private void setChosenOrder(Commande c) {
+        CommandeService cs = new CommandeService();
+
+        String NomProduit = cs.getProductNameById(c.getId_produit());
+        String imageProduit = cs.getProductImageById(c.getId_produit());
+
         selectedOrder = c;
+        id_cmd=c.getId_commande();
+        Image image = new Image(getClass().getResourceAsStream(imageProduit));
+        Image_produit.setImage(image);
+        Date_commande.setStyle("-fx-text-fill: #000000;");
+        Date_commande.setText("Order Date: "+c.getDate_commande());
+        Nom_produit.setText(NomProduit);
+        Montant_total.setText("Total: "+Float.toString(c.getMontant_totale())+" DT");
     }
 
     public void Update() {
@@ -49,7 +81,7 @@ public class ClientOrdersController implements Initializable {
         CommandeService commande = new CommandeService();
         commandesList.clear();
         grid.getChildren().clear();
-        commandesList.addAll(commande.afficherCommandeParIdUser(21));
+        commandesList.addAll(commande.afficherCommandeByUserId(8));
         myListenerCommande = new MyListenerCommande() {
             @Override
             public void onClickListener(Commande c) {
@@ -58,61 +90,54 @@ public class ClientOrdersController implements Initializable {
             }
         };
 
-        int cln = 0;
-        int l = 0;
+        int column = 0;
+        int row = 1;
         try {
-            for (int i = 0; i < commandesList.size(); i++) {
-
-                if (cln > 1) {
-                    cln = 0;
-                    l++;
-                }
+            for (Commande order : commandesList) {
                 FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("/myorders.fxml"));
-
+                fxmlLoader.setLocation(getClass().getResource("/orders.fxml"));
                 AnchorPane anchorPane = fxmlLoader.load();
 
-                MyOrderItemController orderController = fxmlLoader.getController();
-                orderController.setData(commandesList.get(i), myListenerCommande);
+                OrderItemController orderController = fxmlLoader.getController();
+                orderController.setData(order, myListenerCommande);
 
-                grid.add(anchorPane, cln++, l);
-                //grid weight
-                grid.setMinWidth(134);
-                grid.setPrefWidth(134);
-                grid.setMaxWidth(134);//
-                //height
-                grid.setMinHeight(112);
-                grid.setPrefHeight(112);
-                grid.setMaxHeight(112);//
-                grid.setLayoutY(10);
+                if (column == 1) {
+                    column = 0;
+                    row++;
+                }
 
-
-                GridPane.setMargin(anchorPane, new Insets(175, 0, 0, 70));
+                grid.add(anchorPane, column++, row);
+                GridPane.setMargin(anchorPane, new Insets(10));
             }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-    }
-
-    @FXML
-    void GoToShop(ActionEvent event) throws IOException {
-        URL fxURL = getClass().getResource("/ClientShop.fxml");
-        FXMLLoader loader = new FXMLLoader(fxURL);
-        Parent root = (Parent) loader.load();
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("Davincci Admin - Products");
-        stage.setScene(new Scene(root));
-        stage.show();
-
-    }
-
-    @FXML
-    void search(MouseEvent event) {
-        Update();
+        grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
+        grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        scroll.setFitToWidth(true);
+        scroll.setContent(grid);
+        grid.requestLayout();
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Update();
     }
 
+    @FXML
+    void search(ActionEvent event) {
+
+    }
+    @FXML
+    void GoToProducts(ActionEvent event) throws IOException {
+        URL fxURL = getClass().getResource("/ClientProducts.fxml");
+        FXMLLoader loader = new FXMLLoader(fxURL);
+        Parent root = (Parent) loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Davincci - Shop");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
 }
